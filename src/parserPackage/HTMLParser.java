fec41d4378d9387d9da8816835e91a9e7e2d3bf5
tomas-smith <tom.smitas@gmail.com>
2020-08-05 20:55:26 +0100

    uploaded java code

---
+++ src/parserPackage/HTMLParser.java
@@ -0,0 +1,93 @@
+package parserPackage;
+
+
+
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HTMLParser {
+
+
+    public static String parse() throws IOException{
+        Document doc = Jsoup.connect(Main.Input()).get();
+
+        String htmlString = doc.toString();
+        String cleanedHTML = Jsoup.clean(htmlString, Whitelist.none());
+        String normalized;
+        normalized = cleanedHTML.toLowerCase();
+
+
+        //fully remove HTML tags
+        InputStream inputStream = new
+                FileInputStream("en-pos-maxent.bin");
+        POSModel model = new POSModel(inputStream);
+
+        POSTaggerME tagger = new POSTaggerME(model);
+
+
+        WhitespaceTokenizer Tokenizer = WhitespaceTokenizer.INSTANCE;
+        String tokens[] = Tokenizer.tokenize(normalized);
+        String[] tags = tagger.tag(tokens);
+        POSSample sample = new POSSample(tokens, tags);
+        System.out.println("Plaintext with POS applied: ");
+        System.out.println(sample.toString());
+
+        System.out.println("tokens: ");
+        for(String token : tokens){
+            System.out.println(token);
+        }
+
+
+
+
+
+
+        return normalized;
+    }
+    public static String parse2() throws IOException{
+        Document doc = Jsoup.connect(Main.Input2()).get();
+
+        String htmlString = doc.toString();
+        String cleanedHTML = Jsoup.clean(htmlString, Whitelist.none());
+        String normalized;
+        normalized = cleanedHTML.toLowerCase();
+
+
+        //fully remove HTML tags
+        InputStream inputStream = new
+                FileInputStream("en-pos-maxent.bin");
+        POSModel model = new POSModel(inputStream);
+
+        POSTaggerME tagger = new POSTaggerME(model);
+
+
+        WhitespaceTokenizer Tokenizer = WhitespaceTokenizer.INSTANCE;
+        String tokens[] = Tokenizer.tokenize(normalized);
+
+
+        String[] tags = tagger.tag(tokens);
+        POSSample sample = new POSSample(tokens, tags);
+        System.out.println("Plaintext with POS applied: ");
+        System.out.println(sample.toString());
+
+        System.out.println("tokens: ");
+        for(String token : tokens){
+          System.out.println(token);
+        }
+
+
+        return normalized;
+    }
+
+
+
+}
---
+++ src/parserPackage/Main.java
@@ -0,0 +1,54 @@
+package parserPackage;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String Input(){
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Enter desired website: ");
+
+        String websiteName = input.nextLine();
+        System.out.println("Website: " + websiteName);
+
+        return websiteName;
+    }
+
+    public static String Input2(){
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Enter desired website: ");
+
+        String websiteName = input.nextLine();
+        System.out.println("Website: " + websiteName);
+
+        return websiteName;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        String test1 = HTMLParser.parse();
+        String test2 = HTMLParser.parse2();
+
+        List<String> doc1 = Arrays.asList(test1);
+        List<String> doc2 = Arrays.asList(test2);
+
+        List<List<String>> documents = Arrays.asList(doc1, doc2);
+        TFIDF calculator = new TFIDF();
+        double tfidf = calculator.tfIdf(doc1, documents, "the");
+        System.out.println("TF-IDF (ipsum) = " + tfidf);
+
+
+
+
+
+
+
+
+
+    }
+}
